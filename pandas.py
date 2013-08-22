ab7d186aa52de1f5de5bb312cc18f46718591e69
Lars Yencken <lars@yencken.org>
2013-08-22 13:46:41 +1000

    Add snippet to read from MySQL.

--- pandas.py
+++ pandas.py
@@ -3,3 +3,7 @@ pd.read_csv('input.tsv', sep='\t')
 
 # dump to a csv file
 df.to_csv('output.csv', index=False)
+
+# read a frame from MySQL
+conn = MySQL.connect(**kwargs)
+df = pd.io.sql.read_frame("select * from blarg", conn)
